75f347100ae6f8ada3c5255eab09286e2aa91bfc
Rashed Khan Arif <rashed@xeon.technology>
2025-04-24 01:16:26 +0600

    Up

--- src/components/PackageCard.tsx
+++ src/components/PackageCard.tsx
@@ -56,13 +56,47 @@ const PackageCard = ({ pkg }: { pkg: Package }) => {
             </div>
 
             {/* Modal for Package Details */}
-            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} title={pkg.title}>
+            <Modal
+                isOpen={isOpen}
+                onClose={() => setIsOpen(false)}
+                title={pkg.title}>
                 <p className="text-gray-700 mb-2">{pkg.details}</p>
-                <ul className="list-none list-inside text-start text-sm text-gray-600">
-                    {pkg.points.map((point, i) => (
-                        <li className='flex border-[2px] my-1 px-1 py-2 rounded hover:bg-slate-100 border-slate-300 items-center text-lg capitalize' key={i}><FaArrowRight /> <span className='px-2'>{point}</span></li>
-                    ))}
-                </ul>
+                <div className='flex gap-2'>
+                    <div>
+                        <Image
+                            src={pkg.image}
+                            alt={pkg.title}
+                            width={400}
+                            height={250}
+                            className="w-full h-56 object-cover rounded-sm"
+                        />
+                        <div className="pe-4 py-2 text-start">
+                            <div className='flex items-center text-slate-600 space-x-2'>
+                                <FaEuroSign size={16} />
+                                <p className='text-sm'>{pkg.tutionFee}</p>
+                            </div>
+                            <div className='flex items-center mt-2 text-slate-600  space-x-2'>
+                                <FaClock size={16} />
+                                <p className='text-sm'>{pkg.deadLine}</p>
+                            </div>
+                        </div>
+                        <a
+                            href="https://wa.me/+351920682186?text=Hi%2C%20I%27m%20interested%20in%20visa%20consultation"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="w-full mt-2 text-center inline-block bg-green-500 text-white text-sm font-semibold px-2 py-2 rounded-sm shadow hover:bg-[#f2952c] transition"
+                        >
+                            💬 Chat
+                        </a>
+                    </div>
+                    <div>
+                        <ul className="list-none list-inside text-start text-sm text-gray-600">
+                            {pkg.points.map((point, i) => (
+                                <li className='flex border-[1px] my-1 px-1 py-2 rounded hover:bg-slate-100 border-slate-300 items-center text-sm capitalize' key={i}><FaArrowRight size={16} /> <span className='px-2'>{point}</span></li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </Modal>
         </>
     );
