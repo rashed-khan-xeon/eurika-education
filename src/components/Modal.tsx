43b4748c47e671d0f40a248c10d901deefe29089
Rashed Khan Arif <rashed@xeon.technology>
2025-04-23 17:25:46 +0600

    Up

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,9 +13,9 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center px-4">
-            <div className="bg-white rounded-xl shadow-xl max-w-2xl w-full relative">
-                <div className="flex justify-between items-center p-4 border-b">
+        <div className="fixed  inset-0 bg-black/50 z-50 flex items-center justify-center px-4">
+            <div className="bg-white border-amber-600 border-2 rounded-xl shadow-xl max-w-2xl w-full relative">
+                <div className="flex bg-amber-600 justify-between items-center p-4 border-b">
                     <h2 className="text-xl font-semibold">{title}</h2>
                     <button onClick={onClose}>
                         <X className="w-5 h-5 text-gray-500 hover:text-red-500" />
--- src/components/PackageCard.tsx
+++ src/components/PackageCard.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import Modal from './Modal';
 import Image from 'next/image';
 import Chip from './Chip';
-import { FaClock, FaDollarSign, FaMoneyBill } from 'react-icons/fa';
+import { FaArrowRight, FaClock, FaDollarSign, FaMoneyBill } from 'react-icons/fa';
 import { FaTimeline } from 'react-icons/fa6';
 
 type Package = {
@@ -58,10 +58,10 @@ const PackageCard = ({ pkg }: { pkg: Package }) => {
 
             {/* Modal for Package Details */}
             <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} title={pkg.title}>
-                <p className="text-gray-700 mb-4">{pkg.details}</p>
-                <ul className="list-disc list-inside text-sm text-gray-600">
+                <p className="text-gray-700 mb-2">{pkg.details}</p>
+                <ul className="list-none list-inside text-start text-sm text-gray-600">
                     {pkg.points.map((point, i) => (
-                        <li key={i}>{point}</li>
+                        <li className='flex border-[2px] my-1 px-1 py-2 rounded hover:bg-slate-100 border-slate-300 items-center text-lg capitalize' key={i}><FaArrowRight /> <span className='px-2'>{point}</span></li>
                     ))}
                 </ul>
             </Modal>
