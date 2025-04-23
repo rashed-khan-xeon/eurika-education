bb233b5322d7717a4d0526315eade826683d4c9a
Rashed Khan Arif <rashed@xeon.technology>
2025-04-24 00:58:00 +0600

    Up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 // components/Navbar.tsx
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
@@ -6,7 +7,20 @@ const Navbar = () => {
     <header className="sticky top-0 z-50 bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/" className="text-xl font-bold text-blue-600">
-          <span className="text-[#192e67]">Eurika</span> <span className="text-[#f2952c]">Education</span>
+          <div className="flex gap-2 items-center">
+            <div>
+              <Image
+                src={"/images/euredu.png"}
+                alt={"Eurika Education"}
+                width={100}
+                height={50}
+                className="w-full h-8 object-cover"
+              />
+            </div>
+            <div>
+              <span className="text-[#192e67]">Eurika</span> <span className="text-[#f2952c]">Education</span>
+            </div>
+          </div>
         </Link>
         <nav className="space-x-6 hidden md:block">
           <Link href="/" className="hover:text-blue-600">Home</Link>
